5607e26e7587cdda2f85e2bae6e31518aa087306
uknow-Ayush90 <142622441+uknow-Ayush90@users.noreply.github.com>
2025-09-06 19:07:42 +0530

    Time: 480 ms (60.36%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,8 +1,7 @@
 SELECT today.id
 FROM weather AS today
-WHERE EXISTS (
-    SELECT 1
-    FROM weather AS yesterday
-    WHERE today.temperature > yesterday.temperature
-      AND DATEDIFF(today.recordDate, yesterday.recordDate) = 1
-);
+JOIN
+weather AS yesterday
+WHERE today.temperature > yesterday.temperature
+AND DATEDIFF(today.recordDate, yesterday.recordDate) = 1
+
